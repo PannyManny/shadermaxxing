9248b442afba7897dd9b2b8197fc2f1e8810fb2c
PannyManny <panimal.business@gmail.com>
2025-10-11 01:15:53 -0500

    initial setup

---
+++ src/main/java/dev/panimal/pannysandbox/Pannysandbox.java
@@ -0,0 +1,10 @@
+package dev.panimal.pannysandbox;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pannysandbox implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
